062f6679b44f17b2a5a2bfa401cadf2eabba1cc0
damo59 <Damien.Cahill@ittdublin.ie>
2016-09-20 15:02:55 +0100

    Add files via upload

---
+++ Lab1.R
@@ -0,0 +1,8 @@
+n<-1:30
+
+// plot(n^3+n^2+2*n,pch="+",xlab="DataSize",ylab="Time Growth",col="red")
+
+lines(n^2+3*n+1,pch=2,col="blue")
+lines(log(n)+n,col="green")
+lines(n*log(n),col="yellow")
+
