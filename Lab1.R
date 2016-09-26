4564996970e3fee2f3db3dd83880a75ce28e7985
damo59 <Damien.Cahill@ittdublin.ie>
2016-09-26 10:22:12 +0100

    Add files via upload

--- Lab1.R
+++ Lab1.R
@@ -2,6 +2,7 @@ n<-0:30
 m<-c("n^3+n^2+2*n","n^2+3*n+1","log(n)+n","n*log(n)")
 s<-c("red","blue","black","green")
 v<-c(20,8,18,3)
+v1<-c("-","-")
 
 plot(n^3+n^2+2*n,pch=v[1],xlab="DataSize",ylab="Time Growth",col=s[1])
 
@@ -15,3 +16,19 @@ legend(5,26000,col=s,m,pch=v)
 x11()
 plot(n^2+4*n+8,type = "l",xlab="DataSize",ylab="Time Growth",col=s[1])
 lines(n^3,col=s[2])
+legend(1,1000,"n^2+4*n+8\nn^3")
+
+x11()
+plot(n^3+3*n+1,type = "l",xlab="DataSize",ylab="Time Growth",col=s[1])
+lines(n^4,col=s[2])
+legend(1,25000,"n^3+3*n+1 \nn^4")
+
+x11()
+plot(n^2+16*n+11,type = "l",xlab="DataSize",ylab="Time Growth",col=s[1])
+lines(n^3+4*n,col=s[2])
+legend(1,1300,"n^2+16*n+11 \nn^3+4")
+
+x11()
+plot(n^2+4*n+16,type = "l",xlab="DataSize",ylab="Time Growth",col=s[1])
+lines(n^3,col=s[2])
+legend(1,1000,"n^2+4*n+16 \nn^3")
