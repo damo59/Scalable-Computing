a2bac5a1b04dd18c7ec2a15de780da7ef95e901d
damo59 <Damien.Cahill@ittdublin.ie>
2016-10-17 19:50:13 +0100

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main()
 {
+  int intarray[1000000];
+  for (int i = 0; i < arraysz-1; i++)   //parallelism is posible here. domain decomposition.
+
+  {
+
+    intarray[i]= rand()%100;
+
+  }
+  
   
 }
