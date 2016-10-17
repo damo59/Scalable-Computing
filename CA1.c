618fd1249cdf8f0c45dd641371f843a0b82afe6e
John Burns <jzburns@gmail.com>
2016-10-17 23:11:56 +0100

    new files

--- CA1.c
+++ CA1.c
@@ -3,7 +3,7 @@
     CA1 Part 1
    
    The purpose of this program is to compare best and  worst cases of the sort algorithm.
-   The array is filled with randomly placed values, sorted values and the same value in each element.
+   The array is filled with randomly placed values, sorted values and the same value in     each element.
    
    The program is run in each case and the execution time measured.
    For all three cases the execution time should be the same because I believe the complexity 
@@ -21,14 +21,14 @@ int total=0;
 
 int main (int argc, char** argv) {
 
-   srand(time(NULL));
+  // srand(time(NULL));
 
   //this is an integer array of size arraysz
   int a[arraysz];
 
   for (int t = 0; t < arraysz-1; t++) 
   {
-    a[t]= rand()%100;
+    a[t]= 10;
   }
 
 
@@ -38,14 +38,17 @@ int main (int argc, char** argv) {
   {
       for(int j=i; j>=1; j-- )
       {
+	total++;
         if (a[j]<a[j-1])
         {
           int temp=a[j];  
           a[j]=a[j-1];
           a[j-1]=temp;
+	
        }
      }
    } 
+printf("%d",total);
    
  return 1;
 
--- CA1_2.c
+++ CA1_2.c
@@ -20,12 +20,12 @@
  int main()
  {
     srand(time(NULL));
-    int a[20][10], b[20][20], mult2[20][20],i, j, k,r=20,c=20;
+    int a[20][20], b[20][20], mult[20][20],mult2[20][20],i, j, k,r=20,c=20,result=1;
 
 
 
       /* Storing elements of first matrix. */
-#pragma omp parallel for
+#pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
@@ -34,7 +34,7 @@
 
 
  /* Storing elements of second matrix. */
- #pragma omp parallel for
+ #pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
@@ -44,7 +44,7 @@
 
 
  /* Initializing elements of matrix mult to 0.*/
-  #pragma omp parallel for
+  #pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
@@ -61,8 +61,9 @@
      }
   
   /* Multiplying matrix a and b using parallelism and storing in array mult2. */
-  #pragma omp parallel for (private j,k)
+  #pragma omp parallel for private(i,j,k)
      for(i=0; i<r; ++i)
+
      for(j=0; j<c; ++j)
      for(k=0; k<c; ++k)
      {
@@ -78,20 +79,34 @@
          if(j==c-1)
              printf("\n ");
      }
- #pragma omp parallel for 
+printf("\n ");
+
+for(i=0; i<r; ++i)
+     for(j=0; j<c; ++j)
+     {
+         printf("%d ", mult2[i][j]);
+         if(j==c-1)
+             printf("\n ");
+     }
+ //#pragma omp parallel for 
   for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
          
-         if(mult[i][j]!=mult2[i][j])
+         if(mult[i][j]!= mult2[i][j])
+	{
              result=0;
+	}
      }
+
      if (result==1)
         printf("True\n");
      else
         printf("False\n");
-     return 0;
-     return 0;
+
+    
+return 0;
+     
  }
 
 
