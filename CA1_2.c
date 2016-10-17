5338c85db8306799a1784db0bb1b17828145cec4
John Burns <jzburns@gmail.com>
2016-10-17 23:31:52 +0100

    new files

--- CA1_2.c
+++ CA1_2.c
@@ -8,7 +8,7 @@
    Once the matrices are multiplied mult1 is displayed.
    The two matrices are then compared and if they are identical "True" is printed out, if they are not "False" is printed.
    
-   The command to run the program in parallel is
+   The command to compile the program in parallel is g++ -fopenmp CA1_2.c -o CA1_2
    
 */
 
@@ -24,7 +24,8 @@
 
 
 
-      /* Storing elements of first matrix. */
+/* Populate matrix a with random data. */
+
 #pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
@@ -33,7 +34,8 @@
      }
 
 
- /* Storing elements of second matrix. */
+ /* Populate matrix b with random data. */
+
  #pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
@@ -43,16 +45,18 @@
      }
 
 
- /* Initializing elements of matrix mult to 0.*/
+ /* Initializing elements of matrix mult & mult2 to 0.*/
+
   #pragma omp parallel for private(i,j)
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
         mult[i][j]=0;
+	mult2[i][j]=0;
      }
 
 
- /* Multiplying matrix a and b and storing in array mult. */
+ /* Multiplying matrix a and b serially and storing in array mult. */
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      for(k=0; k<c; ++k)
@@ -61,6 +65,7 @@
      }
   
   /* Multiplying matrix a and b using parallelism and storing in array mult2. */
+
   #pragma omp parallel for private(i,j,k)
      for(i=0; i<r; ++i)
 
@@ -70,7 +75,7 @@
          mult2[i][j]+=a[i][k]*b[k][j];
      }
 
- /* Displaying the multiplication of two matrix. */
+ /* Displaying the serial multiplication of the two matrices. */
 
      for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
@@ -81,13 +86,17 @@
      }
 printf("\n ");
 
-for(i=0; i<r; ++i)
+/* Displaying the parallel multiplication of the two matrices. */
+     for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
      {
          printf("%d ", mult2[i][j]);
          if(j==c-1)
              printf("\n ");
      }
+
+/*Comparing the two matrices mult & mult2 to check if they are the same. If a single elememt is different the result is set to 0 and False is printed out.*/
+
  //#pragma omp parallel for 
   for(i=0; i<r; ++i)
      for(j=0; j<c; ++j)
